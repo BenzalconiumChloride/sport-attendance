fcbba8c678a93f9e9d73badc29cf42bc4c25ca26
pedilo01 <146792050+pedilo01@users.noreply.github.com>
2026-02-04 11:37:28 +0800

    Add files via upload

--- database/db_sports.sql
+++ database/db_sports.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2026 at 03:45 AM
+-- Generation Time: Feb 04, 2026 at 04:36 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -76,7 +76,58 @@ CREATE TABLE `bs_user` (
 INSERT INTO `bs_user` (`user_id`, `firstname`, `lastname`, `email`, `password`, `date_added`, `added_by`, `date_modified`, `modified_by`, `date_deleted`, `deleted_by`, `is_deleted`, `last_login`, `uid`) VALUES
 (4, 'Ronald', 'Tangguan', 'ronald@gmail.com', '$2y$10$3L4FOlaoc6tLAtncIJnYgOJJfUMDQdBqSLO.MZIYlIPGa0msBfMWK', '2024-11-26 13:40:23', 1, NULL, 0, NULL, 0, 0, '2025-03-03 06:09:46', 'a87ff679a2f3e71d9181a67b7542122c'),
 (5, 'Benz', 'Lozada', 'benz@gmail.com', '$2y$10$Ak9bkFuEtCGZPIZkF5A4rObu7yF8qh.C0LxTHaksnF5tnkkOHjdQq', '2024-11-26 13:41:04', 1, NULL, 0, NULL, 0, 0, '2026-02-04 02:27:21', 'e4da3b7fbbce2345d7772b0674a318d5'),
-(6, 'Kevin', 'Cortez', 'kevin@gmail.com', '$2y$10$OrZmObNRQApwT4l6llgNZObwWTLSJOImTk4FxRKEDQaD7Gwgmtia.', '2024-11-26 13:43:41', 1, NULL, 0, NULL, 0, 0, '2024-11-26 07:39:16', '1679091c5a880faf6fb5e6087eb1b2dc');
+(6, 'John Lloyd', 'Pedilo', 'Lloyd@gmail.com', '$2y$10$OrZmObNRQApwT4l6llgNZObwWTLSJOImTk4FxRKEDQaD7Gwgmtia.', '2024-11-26 13:43:41', 1, NULL, 0, NULL, 0, 0, '2026-02-04 02:55:55', '1679091c5a880faf6fb5e6087eb1b2dc');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_athlletes`
+--
+
+CREATE TABLE `tbl_athlletes` (
+  `aid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `a_lrn` int(100) NOT NULL,
+  `a_event` varchar(100) NOT NULL,
+  `date_added` varchar(20) NOT NULL,
+  `is_deleted` tinyint(2) NOT NULL DEFAULT 0,
+  `date_deleted` varchar(20) NOT NULL,
+  `deleted_by` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_attendance`
+--
+
+CREATE TABLE `tbl_attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `aid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `check_in_time` datetime NOT NULL,
+  `is_login` int(2) NOT NULL DEFAULT 0,
+  `check_out_time` datetime DEFAULT NULL,
+  `date_recorded` date NOT NULL,
+  `is_deleted` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_coaches`
+--
+
+CREATE TABLE `tbl_coaches` (
+  `cid` int(11) NOT NULL,
+  `c_fullname` varchar(100) NOT NULL,
+  `c_empid` int(100) NOT NULL,
+  `c_event` varchar(100) NOT NULL,
+  `date_added` varchar(20) NOT NULL,
+  `is_deleted` tinyint(2) NOT NULL DEFAULT 0,
+  `date_deleted` varchar(20) NOT NULL,
+  `deleted_by` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -95,6 +146,26 @@ ALTER TABLE `bs_user`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `tbl_athlletes`
+--
+ALTER TABLE `tbl_athlletes`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `tbl_attendance`
+--
+ALTER TABLE `tbl_attendance`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `aid` (`aid`),
+  ADD KEY `cid` (`cid`);
+
+--
+-- Indexes for table `tbl_coaches`
+--
+ALTER TABLE `tbl_coaches`
+  ADD PRIMARY KEY (`cid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -109,6 +180,24 @@ ALTER TABLE `bs_setting`
 --
 ALTER TABLE `bs_user`
   MODIFY `user_id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_athlletes`
+--
+ALTER TABLE `tbl_athlletes`
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_attendance`
+--
+ALTER TABLE `tbl_attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_coaches`
+--
+ALTER TABLE `tbl_coaches`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
